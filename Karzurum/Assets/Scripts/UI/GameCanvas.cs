8642e319ed2ccb15c74d59922410ebe3d7fe4ce3
ATLGAN <sak.atilgan@gmail.com>
2021-03-08 23:33:50 +0300

    Made main menu

--- Karzurum/Assets/Scripts/UI/GameCanvas.cs
+++ Karzurum/Assets/Scripts/UI/GameCanvas.cs
@@ -7,20 +7,28 @@ public class GameCanvas : MonoBehaviour
 {
     public string gameScene;
 
+    public SettingsPanel settingsPanel;
+    public GameObject creditPanel;
+
+    private void OnEnable()
+    {
+        settingsPanel.gameObject.SetActive(false);
+        creditPanel.gameObject.SetActive(false);
+    }
     public void Pressed_StartButton()
     {
         SceneManager.LoadScene(gameScene);
     }
     public void Pressed_SettingsButton()
     {
-
+        settingsPanel.gameObject.SetActive(true);
     }
     public void Pressed_CreditButton()
     {
-
+        creditPanel.SetActive(true);
     }
     public void Pressed_ExitButton()
     {
-
+        Application.Quit();
     }
 }
---
+++ Karzurum/Assets/Scripts/UI/SettingsPanel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsPanel : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+    public void Pressed_ExitButton()
+    {
+        gameObject.SetActive(false);
+    }
+    public void ChangedQuailtyDropdown(int value)
+    {
+        QualitySettings.SetQualityLevel(value);
+
+
+    }
+}
